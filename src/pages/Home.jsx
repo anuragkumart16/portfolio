452b0e236e7ba53ae4b09d438b99edd8031e1cd9
anuragkumart16 <114322758+anuragkumart16@users.noreply.github.com>
2025-02-04 13:12:39 +0530

    added changes'

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import Navbar from "./components/navbar/Navbar";
+
+export default function Home() {
+    return (
+        <>
+            <Navbar />
+            <div className="homeContainer">
+                <h1>Home</h1>
+            </div>
+        </>
+    )
+}
